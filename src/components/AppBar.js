0f916492e57b78f0368e6760cd3c7a761520b36e
Gr34sy <slupek3@poczta.fm>
2024-04-20 19:21:59 +0200

    finished appbar styling

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -6,10 +6,12 @@ import {
   faChevronLeft,
   faChevronRight,
   faGear,
+  faHouse,
+  faToggleOff,
+  faToggleOn,
+  faSmog,
 } from "@fortawesome/free-solid-svg-icons";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faCalendar } from "@fortawesome/free-regular-svg-icons";
-import { faSmog } from "@fortawesome/free-solid-svg-icons";
 
 // Components
 import Link from "next/link";
@@ -23,44 +25,131 @@ export function AppBar() {
 
   return (
     <aside
-      className={"bg-white-100 py-5 rounded-xl " + (collapsed ? "" : "max-w-[180px]")}
+      className={
+        "bg-white-100 py-5 rounded-xl flex flex-col gap-4"
+      }
     >
-      <div onClick={() => setCollapsed((prev) => !prev)}>
+      <div
+        className={"px-3 flex items-center " + (!collapsed ? "justify-between gap-1" : "justify-center")}
+        onClick={() => setCollapsed((prev) => !prev)}
+      >
+        {!collapsed && <div className="flex gap-2 hover:cursor-pointer">
+          <FontAwesomeIcon
+            icon={faToggleOff}
+            className="size-[25px] hover:cursor-pointer text-slate-700"
+          />
+
+          <p className="font-semibold tracking-wider">Dark</p>
+        </div>}
+
         <FontAwesomeIcon
           icon={collapsed ? faChevronRight : faChevronLeft}
-          className="size-[20px] hover:cursor-pointer"
+          className="size-[25px] hover:cursor-pointer text-slate-700"
         />
       </div>
 
       <ul className="flex flex-col gap-3">
         <li>
-          <Link href="/" className={"flex px-3 gap-2 items-center text-lg " + (collapsed && active==="homepage" ? "border-r-8 border-solid border-red-400" : "")}>
-            <FontAwesomeIcon icon={faHouse} className="size-[35px] my-[5px]" />
-            {!collapsed && <p className={"px-2 py-2 rounded-2xl " + (active === "homepage" ? "text-white-100 bg-red-400" : "")}>Homepage</p>}
+          <Link
+            href="/"
+            className={
+              "flex px-3 gap-2 items-center text-lg " +
+              (collapsed && active === "homepage"
+                ? "border-r-8 border-solid border-red-400"
+                : "")
+            }
+          >
+            <FontAwesomeIcon icon={faHouse} className="size-[35px] my-[5px] text-slate-700" />
+            {!collapsed && (
+              <p
+                className={
+                  "px-2 py-2 rounded-2xl " +
+                  (active === "homepage" ? "text-white-100 bg-red-400" : "")
+                }
+              >
+                Homepage
+              </p>
+            )}
           </Link>
         </li>
 
         <li>
-          <Link href="/30forecast" className={"flex px-3 gap-2 items-center text-lg " + (collapsed && active==="30forecast" ? "border-r-8 border-solid border-red-400" : "")}>
-            <FontAwesomeIcon icon={faCalendar} className="size-[35px] my-[5px]" />
-            {!collapsed && <p className={"px-2 py-2 rounded-2xl " + (active === "30forecast" ? "text-white-100 bg-red-400" : "")}>Forecast</p>}
+          <Link
+            href="/30forecast"
+            className={
+              "flex px-3 gap-2 items-center text-lg " +
+              (collapsed && active === "30forecast"
+                ? "border-r-8 border-solid border-red-400"
+                : "")
+            }
+          >
+            <FontAwesomeIcon
+              icon={faCalendar}
+              className="size-[35px] my-[5px] text-slate-700"
+            />
+            {!collapsed && (
+              <p
+                className={
+                  "px-2 py-2 rounded-2xl " +
+                  (active === "30forecast" ? "text-white-100 bg-red-400" : "")
+                }
+              >
+                Forecast
+              </p>
+            )}
           </Link>
         </li>
 
         <li>
-          <Link href="/pollution" className={"flex px-3 gap-2 items-center text-lg " + (collapsed && active==="pollution" ? "border-r-8 border-solid border-red-400" : "")}>
-            <FontAwesomeIcon icon={faSmog} className="size-[35px] my-[5px]" />
-            {!collapsed && <p className={"px-2 py-2 rounded-2xl " + (active === "pollution" ? "text-white-100 bg-red-400" : "")}>Pollution</p>}
+          <Link
+            href="/pollution"
+            className={
+              "flex px-3 gap-2 items-center text-lg " +
+              (collapsed && active === "pollution"
+                ? "border-r-8 border-solid border-red-400"
+                : "")
+            }
+          >
+            <FontAwesomeIcon icon={faSmog} className="size-[35px] my-[5px] text-slate-700" />
+            {!collapsed && (
+              <p
+                className={
+                  "px-2 py-2 rounded-2xl " +
+                  (active === "pollution" ? "text-white-100 bg-red-400" : "")
+                }
+              >
+                Pollution
+              </p>
+            )}
           </Link>
         </li>
 
         <li>
-          <Link href="/options" className={"flex px-3 gap-2 items-center text-lg " + (collapsed && active==="options" ? "border-r-8 border-solid border-red-400" : "")}>
-            <FontAwesomeIcon icon={faGear} className="size-[35px] my-[5px]" />
-            {!collapsed && <p className={"px-2 py-2 rounded-2xl " + (active === "options" ? "text-white-100 bg-red-400" : "")}>Options</p>}
+          <Link
+            href="/options"
+            className={
+              "flex px-3 gap-2 items-center text-lg " +
+              (collapsed && active === "options"
+                ? "border-r-8 border-solid border-red-400"
+                : "")
+            }
+          >
+            <FontAwesomeIcon icon={faGear} className="size-[35px] my-[5px] text-slate-700" />
+            {!collapsed && (
+              <p
+                className={
+                  "px-2 py-2 rounded-2xl " +
+                  (active === "options" ? "text-white-100 bg-red-400" : "")
+                }
+              >
+                Options
+              </p>
+            )}
           </Link>
         </li>
       </ul>
+
+      {collapsed && <FontAwesomeIcon icon={faToggleOff} className="px-3 pt-3 size-[35px] text-slate-700 hover:cursor-pointer"/>}
     </aside>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { faMeteor } from "@fortawesome/free-solid-svg-icons";
 export function Header() {
   return (
     <header className="absolute top-0 left-1/2 translate-x-[-50%] flex gap-1 items-center bg-red-400 rounded-b-3xl py-2 px-4 sm:px-20">
-      <FontAwesomeIcon icon={faMeteor} className="size-[50px] text-red-700" />
+      <FontAwesomeIcon icon={faMeteor} className="size-[50px] text-slate-700" />
 
       <p className="text-[2rem] text-white-100">MeteorWeather</p>
     </header>
