d0b3c2db1f335ce0f1cb36b84af05f6c4678f6b5
Gr34sy <slupek3@poczta.fm>
2024-04-20 16:20:36 +0200

    ready header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,10 @@
 import { Inter } from "next/font/google";
+import "./reset.css";
 import "./globals.css";
+
+import { AppBar } from "@/components/AppBar";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +16,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <AppBar />
+        <div className="layout bg-white-300">{children}</div>
+        <Footer />
+      </body>
     </html>
   );
 }
