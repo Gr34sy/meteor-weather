90e0eeebe0783d71ead0b413ebaa3b280005fbc9
Gr34sy <slupek3@poczta.fm>
2024-04-25 00:23:07 +0200

    changed nesting leyout children

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <Layout children={children}/>
+        <Layout>
+          {children}
+        </Layout>
       </body>
     </html>
   );
